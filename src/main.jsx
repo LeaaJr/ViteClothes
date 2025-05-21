058799c4cb014e698254129b927cb6f6a9e94a5c
LeaaJr <leeitooocabj@gmail.com>
2025-05-22 01:18:56 +0200

    fixed_cart

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './auth/AuthContext.jsx';
-import { CartProvider } from './context/CartContext';
+import CartProvider from './context/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
